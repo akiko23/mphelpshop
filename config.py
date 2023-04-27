fd610ead540ce70b32112b22f5f4df97fc6b0b3f
Dmitriy Vasilenko <101889433+akiko23@users.noreply.github.com>
2023-04-27 23:52:12 +0300

    security👮

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import os
 
 from db import Database
 
 
-BOT_TOKEN = "5875663139:AAFACUhFV5bNoUel6C1E7KbdUP2GFlTkyI4"
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 DB_FILE = 'db.sqlite3'
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher(bot=bot, storage=MemoryStorage())
 db = Database(DB_FILE)
 
-secretKey = '5ad8d388d4fbf52d6c4e539d6b3248a0'.encode('utf-8')
+secretKey = os.environ.get('SECRET_KEY').encode('utf-8')
 
